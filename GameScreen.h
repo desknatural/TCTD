89a474c2254dccff7af34ebfadf1585de569cdf1
desknatural <ap469@cornell.edu>
2013-09-05 14:25:25 -0400

    First Project Files Upload
    Experimental first upload of files, not quite sure what I'm doing but
    hopefully this is going in the right direction!

---
+++ GameScreen.h
@@ -0,0 +1,15 @@
+// Header file for Tower Defense.cpp
+#ifndef GAMESCREEN_H
+#define GAMESCREEN_H
+#include<SFML/Graphics.hpp>
+
+class gameScreen : public sf::Drawable{
+public:
+	virtual void eventParse(sf::Event) = 0;
+	virtual gameScreen* nextScreen();
+	virtual ~gameScreen();
+private:
+	virtual void draw(sf::RenderTarget&, sf::RenderStates) const=0;
+};
+
+#endif
---
+++ Map.h
@@ -0,0 +1,15 @@
+#ifndef	MAP_H_INCLUDED
+#define MAP_H_INCLUDED
+
+// Monolithic Map Class
+
+
+class Map {
+private:
+	int score, Lives;
+
+
+
+};
+
+#endif
